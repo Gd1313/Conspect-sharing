fb5edad37afa0776008929626554f611f8ba1bb4
student <student@STDNT-02-1-1-K1>
2018-09-21 22:06:31 +0300

    initial commit

---
+++ Conspect-sharing/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Conspect_sharing.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
