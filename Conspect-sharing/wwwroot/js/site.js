c762f0232e1d8625311891b2d5edd7af0553b343
student <student@STDNT-02-1-1-K1>
2018-09-26 20:28:54 +0300

    add theme change intermediate commit

--- Conspect-sharing/wwwroot/js/site.js
+++ Conspect-sharing/wwwroot/js/site.js
@@ -1,83 +1,8 @@
-﻿// Write your JavaScript code.
-//$('#select_all').click(function () {
-//    var c = this.checked;
-//    $(':checkbox').prop('checked', c);
-//});
-//function Locking() {
-//    $.ajax({
-//        type: 'POST',
-//        url: "@Url.Action("LockUser", "Account")",
-//        data: { arr: getCheckedCheckBoxes() },
-//        success: function (data) {
-//            if (data == true) {
-//                location.reload();
-//            }
-//            else {
-//            }
-//        },
-//        error: function () {
-//            alert("Error");
-//        }
-//    });
-//}
-//function Unlocking() {
-//    $.ajax({
-//        type: 'POST',
-//        url: "@Url.Action("UnLockUser", "Account")",
-//        data: { arr: getCheckedCheckBoxes() },
-//        success: function (data) {
-//            if (data == true) {
-//                location.reload();
-//            }
-//            else {
-//            }
-//        },
-//        error: function () {
-//            alert("Error");
-//        }
-//    });
-//}
-//function Delete() {
-//    $.ajax({
-//        type: 'POST',
-//        url: "@Url.Action("DeleteUser", "Account")",
-//        data: { arr: getCheckedCheckBoxes() },
-//        success: function (data) {
-//            if (data == true) {
-//                location.reload();
-//            }
-//            else {
-//            }
-//        },
-//        error: function () {
-//            alert("Error");
-//        }
-//    });
-//}
-
-//function getCheckedCheckBoxes() {
-//    var checkboxes = document.getElementsByClassName('checkbox');
-//    var checkboxesChecked = [];
-//    for (var index = 0; index < checkboxes.length; index++) {
-//        if (checkboxes[index].checked) {
-//            checkboxesChecked.push(checkboxes[index].value);
-//        }
-//    }
-//    return checkboxesChecked;
-//}
+﻿
     var themes = {
 
-        "default": "lib/bootstrap/dist/css/bootstrap.min.css",
-    "amelia": "lib/bootswatch/amelia/bootstrap.min.css",
-    "cerulean": "/lib/bootswatch/cerulean/bootstrap.min.css",
+    "default": "lib/bootstrap/dist/css/bootstrap.min.css",
     "cosmo": "lib/bootswatch/cosmo/bootstrap.min.css",
-    "cyborg": "lib/bootswatch/cyborg/bootstrap.min.css",
-    "flatly": "lib/bootswatch/flatly/bootstrap.min.css",
-    "journal": "lib/bootswatch/journal/bootstrap.min.css",
-    "readable": "lib/bootswatch/readable/bootstrap.min.css",
-    "simplex": "lib/bootswatch/simplex/bootstrap.min.css",
-    "slate": "lib/bootswatch/slate/bootstrap.min.css",
-    "spacelab": "lib/bootswatch/spacelab/bootstrap.min.css",
     "united": "lib/bootswatch/superhero/bootstrap.min.css"
 }
         $(function () {
@@ -85,6 +10,41 @@
     themesheet.appendTo('head');
             $('.theme-link').click(function () {
                 var themeurl = themes[$(this).attr('data-theme')];
-    themesheet.attr('href', themeurl);
+                themesheet.attr('href', themeurl);
+        if (supports_storage) {
+            localStorage.theme = themeurl;
+        }
 });
 });
+
+function supports_html5_storage() {
+    try {
+        return 'localStorage' in window && window['localStorage'] !== null;
+    } catch (e) {
+        return false;
+    }
+}
+
+var supports_storage = supports_html5_storage();
+
+function set_theme(theme) {
+    //$('link[title="main"]').attr('href', theme);
+    if (supports_storage) {
+        localStorage.theme = theme;
+    }
+    console.log(localStorage.theme);
+}
+
+/* On load, set theme from local storage */
+if (supports_storage) {
+    var themesheet = $('<link href="' + themes['default'] + '" rel="stylesheet" />');
+    themesheet.appendTo('head');
+    console.log(localStorage.theme);
+    var theme = localStorage.theme;
+    if (theme) {
+        themesheet.attr('href', theme);
+    } else {
+        /* Don't annoy user with options that don't persist */
+        //$('#theme-dropdown').hide();
+    }
+}
